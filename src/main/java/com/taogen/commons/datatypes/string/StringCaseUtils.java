ecdc8ef9288b518012e8a63a2e55d13dfcce8b99
taogen <taogenjia@gmail.com>
2021-08-03 23:10:54 +0800

    [feat] Add StringCaseUtils

---
+++ src/main/java/com/taogen/commons/datatypes/string/StringCaseUtils.java
@@ -0,0 +1,112 @@
+package com.taogen.commons.datatypes.string;
+
+/**
+ * Common Case:
+ * - snake_case
+ * - kebab-case
+ * - PascalCase
+ * - camelCase
+ * - lowercase
+ * - UPPERCASE
+ *
+ * @author Taogen
+ */
+public class StringCaseUtils {
+    public StringCaseUtils() {
+        throw new IllegalStateException("Can't instantiate the utility class");
+    }
+
+    /**
+     * Convert String from snake case to camel case
+     *
+     * @param source start with letter
+     * @return
+     */
+    public static String snakeCaseToCamelCase(String source) {
+        if (source == null || source.isEmpty()) {
+            return source;
+        }
+        return convertAnyCaseToCamelCase(source, "_");
+    }
+
+    public static String kebabCaseToCamelCase(String source) {
+        if (source == null || source.isEmpty()) {
+            return source;
+        }
+        return convertAnyCaseToCamelCase(source, "-");
+    }
+
+    public static String convertAnyCaseToCamelCase(String source, String joinChar) {
+        source = source.toLowerCase();
+        int i = 0;
+        while (i < source.length()) {
+            int index = source.indexOf(joinChar, i);
+            if (index == -1) {
+                return source;
+            } else if (index == source.length() - 1) {
+                source = source.replace(joinChar, "");
+            } else {
+                String targetString = source.substring(index, index + 2);
+                String replacementString = source.substring(index + 1, index + 2);
+                source = source.replaceFirst(targetString, replacementString.toUpperCase());
+            }
+            i = index + 1;
+        }
+        return source;
+    }
+
+    /**
+     * Convert String from snake case to pascal case
+     *
+     * @param source start with letter
+     * @return
+     */
+    public static String snakeCaseToPascalCase(String source) {
+        if (source == null || source.isEmpty()) {
+            return source;
+        }
+        return covertAnyCaseToPascalCase(source, "_");
+    }
+
+    public static String kebabCaseToPascalCase(String source) {
+        if (source == null || source.isEmpty()) {
+            return source;
+        }
+        return covertAnyCaseToPascalCase(source, "-");
+    }
+
+    private static String covertAnyCaseToPascalCase(String source, String joinChar) {
+        source = source.toLowerCase();
+        String firstLetter = source.substring(0, 1);
+        source = source.replaceFirst(firstLetter, firstLetter.toUpperCase());
+        int i = 0;
+        while (i < source.length()) {
+            int index = source.indexOf(joinChar, i);
+            if (index == -1) {
+                return source;
+            } else if (index == source.length() - 1) {
+                source = source.replace(joinChar, "");
+            } else {
+                String targetString = source.substring(index, index + 2);
+                String replacementString = source.substring(index + 1, index + 2);
+                source = source.replaceFirst(targetString, replacementString.toUpperCase());
+            }
+            i = index + 1;
+        }
+        return source;
+    }
+
+    public static String firstLetterToUpperCase(String source) {
+        if (source == null || source.isEmpty()) {
+            return source;
+        }
+        return source.substring(0, 1).toUpperCase() + source.substring(1);
+    }
+
+    public static String firstLetterToLowerCase(String source) {
+        if (source == null || source.isEmpty()) {
+            return source;
+        }
+        return source.substring(0, 1).toLowerCase() + source.substring(1);
+    }
+}
---
+++ src/test/java/com/taogen/commons/datatypes/string/StringCaseUtilsTest.java
@@ -0,0 +1,65 @@
+package com.taogen.commons.datatypes.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class StringCaseUtilsTest {
+
+    @Test
+    void snakeCaseToCamelCase() {
+        assertNull(StringCaseUtils.snakeCaseToCamelCase(null));
+        assertEquals("", StringCaseUtils.snakeCaseToCamelCase(""));
+        assertEquals("aBC", StringCaseUtils.snakeCaseToCamelCase("a_b_c")); // aBC
+        assertEquals("abc", StringCaseUtils.snakeCaseToCamelCase("abc")); // abc
+    }
+
+    @Test
+    void kebabCaseToCamelCase() {
+        assertNull(StringCaseUtils.kebabCaseToCamelCase(null));
+        assertEquals("", StringCaseUtils.kebabCaseToCamelCase(""));
+        assertEquals("aBC", StringCaseUtils.kebabCaseToCamelCase("a-b-c"));
+        assertEquals("abc", StringCaseUtils.kebabCaseToCamelCase("abc"));
+    }
+
+    @Test
+    void convertAnyCaseToCamelCase() {
+    }
+
+    @Test
+    void snakeCaseToPascalCase() {
+        assertNull(StringCaseUtils.snakeCaseToPascalCase(null));
+        assertEquals("", StringCaseUtils.snakeCaseToPascalCase(""));
+        assertEquals("ABC", StringCaseUtils.snakeCaseToPascalCase("a_b_c")); // ABC
+        assertEquals("Abc", StringCaseUtils.snakeCaseToPascalCase("abc")); // Abc
+    }
+
+    @Test
+    void kebabCaseToPascalCase() {
+        assertNull(StringCaseUtils.kebabCaseToPascalCase(null));
+        assertEquals("", StringCaseUtils.kebabCaseToPascalCase(""));
+        assertEquals("ABC", StringCaseUtils.kebabCaseToPascalCase("a-b-c"));
+        assertEquals("Abc", StringCaseUtils.kebabCaseToPascalCase("abc"));
+    }
+
+    @Test
+    void firstLetterToUpperCase() {
+        assertNull(StringCaseUtils.firstLetterToUpperCase(null));
+        assertEquals("", StringCaseUtils.firstLetterToUpperCase(""));
+        assertEquals("A", StringCaseUtils.firstLetterToUpperCase("A")); // A
+        assertEquals("A", StringCaseUtils.firstLetterToUpperCase("a")); // A
+        assertEquals("AA", StringCaseUtils.firstLetterToUpperCase("aA")); // AA
+        assertEquals("AA", StringCaseUtils.firstLetterToUpperCase("AA")); // AA
+    }
+
+    @Test
+    void firstLetterToLowerCase() {
+        assertNull(StringCaseUtils.firstLetterToLowerCase(null));
+        assertEquals("", StringCaseUtils.firstLetterToLowerCase(""));
+        assertEquals("a", StringCaseUtils.firstLetterToLowerCase("a")); // a
+        assertEquals("a", StringCaseUtils.firstLetterToLowerCase("A")); // a
+        assertEquals("aa", StringCaseUtils.firstLetterToLowerCase("Aa")); // aa
+        assertEquals("aa", StringCaseUtils.firstLetterToLowerCase("aa")); // aa
+    }
+}
