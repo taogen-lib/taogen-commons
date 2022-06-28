bec4713e3601c610916cfa67d67dfe21c07b0e60
taogen <taogenjia@gmail.com>
2022-06-28 10:35:05 +0800

    [refactor] Update method names

--- src/main/java/com/taogen/commons/crypto/EncodeUtils.java
+++ src/main/java/com/taogen/commons/crypto/EncodeUtils.java
@@ -8,12 +8,12 @@ import java.util.Base64;
  * @author Taogen
  */
 public class EncodeUtils {
-    public static String toBase64(String source) {
+    public static String base64Encode(String source) {
         return Base64.getEncoder().encodeToString(
                 source.getBytes(StandardCharsets.UTF_8));
     }
 
-    public static String fromBase64(String source) {
+    public static String base64Decode(String source) {
         return new String(Base64.getDecoder().decode(source),
                 StandardCharsets.UTF_8);
     }
