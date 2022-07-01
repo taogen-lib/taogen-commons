f2304dea7c735797130b007cdbb1f5f2196cfd80
taogen <taogenjia@gmail.com>
2022-07-01 22:34:26 +0800

    [refactor] Optimize RandomUtils

--- src/main/java/com/taogen/commons/RandomUtils.java
+++ src/main/java/com/taogen/commons/RandomUtils.java
@@ -1,38 +1,47 @@
 package com.taogen.commons;
 
 import com.taogen.commons.datatypes.string.StringUtils;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class RandomUtils
-{
+@Log4j2
+public class RandomUtils {
 
-    public static int getRandomNumber(int bound)
-    {
-        Random random = new Random();
-        return random.nextInt(bound);
+    /**
+     * Random is thread safe for use by multiple threads. But if multiple threads use the same instance of Random, the same seed is shared by multiple threads. It leads to contention between multiple threads and so to performance degradation.
+     * return 0 <= x < bound
+     *
+     * @param bound
+     * @return
+     */
+    public static int getRandomNumber(int bound) {
+//        Random random = new Random();
+//        int randomNum = random.nextInt(bound);
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int randomNum = random.nextInt(0, bound);
+        log.debug("randomNum is {}", randomNum);
+        return randomNum;
     }
 
-    public static String getRandomNumberStr(int length)
-    {
-        if (length <= 0)
-        {
+    public static String getRandomNumberStr(int length) {
+        if (length <= 0) {
             return null;
         }
         Random random = new Random();
-        int bound = ((Double)Math.pow(10, length)).intValue();
-        return StringUtils.expandLength(String.valueOf(random.nextInt(bound)), length,'0');
+        int bound = ((Double) Math.pow(10, length)).intValue();
+        return StringUtils.expandLength(String.valueOf(random.nextInt(bound)), length, '0');
     }
 
     /**
      * Get Random from A-Z 0-9
+     *
      * @param length
      * @return
      */
-    public static String getMixedRandomStr(int length)
-    {
-        if (length <= 0)
-        {
+    public static String getMixedRandomStr(int length) {
+        if (length <= 0) {
             return null;
         }
         int ascii_A = 65;
@@ -41,11 +50,10 @@ public class RandomUtils
         int ascii_9 = 57;
         int size = (ascii_Z - ascii_A + 1) + (ascii_9 - ascii_0 + 1);
         StringBuilder result = new StringBuilder(length);
-        for (int i = 0; i < length; i++)
-        {
+        for (int i = 0; i < length; i++) {
             int random = getRandomNumber(size);
             random = random > 9 ? random + ascii_A - 10 : random + ascii_0;
-            result.append((char)(random));
+            result.append((char) (random));
         }
         return result.toString();
     }
--- src/test/java/com/taogen/commons/RandomUtilsTest.java
+++ src/test/java/com/taogen/commons/RandomUtilsTest.java
@@ -2,28 +2,30 @@ package com.taogen.commons;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RandomUtilsTest {
 
-    @Test 
-    public void getRandomNumberTest()
-    {
-        int bound1 = 20;
-        assertTrue(RandomUtils.getRandomNumber(bound1) < bound1 && RandomUtils.getRandomNumber(bound1) > 0);
+    @Test
+    public void getRandomNumberTest() {
+        int bound = 1;
+        int randomNumber = RandomUtils.getRandomNumber(bound);
+        assertTrue(randomNumber >= 0 && randomNumber < bound);
+        bound = 2;
+        int randomNumber2 = RandomUtils.getRandomNumber(bound);
+        assertTrue(randomNumber2 >= 0 && randomNumber2 < bound);
     }
 
     @Test
-    public void getRandomNumberStrTest()
-    {
+    public void getRandomNumberStrTest() {
         assertNull(RandomUtils.getRandomNumberStr(0));
         assertNull(RandomUtils.getRandomNumberStr(-1));
         assertTrue(RandomUtils.getRandomNumberStr(10).matches("^[0-9]{10}$"));
     }
 
     @Test
-    public void getMixedRandomStrTest()
-    {
+    public void getMixedRandomStrTest() {
         assertNull(RandomUtils.getMixedRandomStr(0));
         assertNull(RandomUtils.getMixedRandomStr(-1));
         assertTrue(RandomUtils.getMixedRandomStr(10).matches("^[0-9A-Z]{10}$"));
