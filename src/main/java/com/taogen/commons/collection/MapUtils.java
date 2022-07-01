3dbf50a1129d8c7daa6b58d0dfb1339dd159dd61
taogen <taogenjia@gmail.com>
2022-07-01 22:11:26 +0800

    [feat] Add methods of MapUtils

--- src/main/java/com/taogen/commons/collection/MapUtils.java
+++ src/main/java/com/taogen/commons/collection/MapUtils.java
@@ -1,22 +1,24 @@
 package com.taogen.commons.collection;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * @author Taogen
  */
+@Log4j2
 public class MapUtils {
     public static Map<String, List<Object>> multiStringValueMapToMultiObjectValueMap(Map<String, List<String>> multiStringValueMap) {
         if (multiStringValueMap == null) {
             return null;
         }
-        Map<String, List<Object>> multiObjectValueMap = new HashMap<>();
+        Map<String, List<Object>> multiObjectValueMap = new HashMap<>(multiStringValueMap.size());
         multiStringValueMap.forEach((key, value) -> {
             List<Object> objectList = null;
             if (value != null) {
-                objectList = value.stream()
-                        .collect(Collectors.toList());
+                objectList = new ArrayList<>(value);
             }
             multiObjectValueMap.put(key, objectList);
         });
@@ -27,7 +29,7 @@ public class MapUtils {
         if (multiObjectValueMap == null) {
             return null;
         }
-        Map<String, List<String>> multiStringValueMap = new HashMap<>();
+        Map<String, List<String>> multiStringValueMap = new HashMap<>(multiObjectValueMap.size());
         multiObjectValueMap.forEach((key, value) -> {
             List<String> stringList = null;
             if (value != null) {
@@ -39,4 +41,60 @@ public class MapUtils {
         });
         return multiStringValueMap;
     }
+
+    public static boolean multiValueMapEquals(Map<String, List<Object>> a, Map<String, List<Object>> b) {
+        if (a == null) {
+            a = Collections.emptyMap();
+        }
+        if (b == null) {
+            b = Collections.emptyMap();
+        }
+        if (a.isEmpty() && b.isEmpty()) {
+            return true;
+        }
+        if (a.size() != b.size()) {
+            return false;
+        }
+        Set<String> keys = a.keySet();
+        for (String key : keys) {
+            log.debug("key is {}", key);
+            List<?> aValues = a.get(key);
+            if (aValues == null) {
+                aValues = Collections.emptyList();
+            }
+            List<?> bValues = b.get(key);
+            if (bValues == null) {
+                bValues = Collections.emptyList();
+            }
+            if (aValues.isEmpty() && bValues.isEmpty()) {
+                continue;
+            }
+            if (aValues.size() != bValues.size()) {
+                return false;
+            }
+            log.debug("before sort: aValue is {}", aValues);
+            log.debug("before sort: bValue is {}", bValues);
+            Collections.sort(aValues, Comparator.comparing(item ->
+                            Objects.toString(item, null),
+                    Comparator.nullsLast(Comparator.naturalOrder())));
+            Collections.sort(bValues, Comparator.comparing(item ->
+                            Objects.toString(item, null),
+                    Comparator.nullsLast(Comparator.naturalOrder())));
+            log.debug("after sort: aValue is {}", aValues);
+            log.debug("after sort: bValue is {}", bValues);
+            for (int i = 0; i < aValues.size(); i++) {
+                if (!String.valueOf(aValues.get(i)).equals(String.valueOf(bValues.get(i)))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean multiStringValueMapEquals(Map<String, List<String>> a, Map<String, List<String>> b) {
+        Map<String, List<Object>> aMultiObjectValueMap = multiStringValueMapToMultiObjectValueMap(a);
+        Map<String, List<Object>> bMultiObjectValueMap = multiStringValueMapToMultiObjectValueMap(b);
+        return multiValueMapEquals(aMultiObjectValueMap, bMultiObjectValueMap);
+    }
+
 }
---
+++ src/test/java/com/taogen/commons/collection/MapUtilsTest.java
@@ -0,0 +1,99 @@
+package com.taogen.commons.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MapUtilsTest {
+
+    @Test
+    void multiStringValueMapToMultiObjectValueMap() {
+        // TODO
+    }
+
+    @Test
+    void multiObjectValueMapToMultiStringValueMap() {
+        // TODO
+    }
+
+    /**
+     *
+     */
+    @Test
+    void multiValueMapEquals() {
+        String key = null;
+        // null null -> true
+        assertTrue(MapUtils.multiValueMapEquals(null, null));
+        // null empty -> true
+        assertTrue(MapUtils.multiValueMapEquals(null, Collections.emptyMap()));
+        // empty null -> true
+        assertTrue(MapUtils.multiValueMapEquals(Collections.emptyMap(), null));
+        // empty empty -> true
+        assertTrue(MapUtils.multiValueMapEquals(Collections.emptyMap(), Collections.emptyMap()));
+        // map size is not equal -> false
+        key = "map size is not equal -> false";
+        Map<String, List<Object>> map1 = new HashMap<>();
+        map1.put(key, null);
+        map1.put(key + "2", null);
+        Map<String, List<Object>> map2 = new HashMap<>();
+        map2.put(key, null);
+        assertFalse(MapUtils.multiValueMapEquals(map1, map2));
+        // list=null list=null -> true
+        key = "list=null list=null -> true";
+        map1.clear();
+        map1.put(key, null);
+        map2.clear();
+        map2.put(key, null);
+        assertTrue(MapUtils.multiValueMapEquals(map1, map2));
+        // list=null list=empty -> true
+        key = "list=null list=empty -> true";
+        map1.clear();
+        map1.put(key, Collections.emptyList());
+        map2.clear();
+        map2.put(key, null);
+        assertTrue(MapUtils.multiValueMapEquals(map1, map2));
+        // list=empty list=empty -> true
+        key = "list=empty list=empty -> true";
+        map1.clear();
+        map1.put(key, Collections.emptyList());
+        map2.clear();
+        map2.put(key, Collections.emptyList());
+        assertTrue(MapUtils.multiValueMapEquals(map1, map1));
+        // list size is not equal -> false
+        key = "list size is not equal -> false";
+        map1.clear();
+        map1.put(key, new ArrayList<>(Arrays.asList(1, 2)));
+        map2.clear();
+        map2.put(key, new ArrayList<>(Arrays.asList(1)));
+        assertFalse(MapUtils.multiValueMapEquals(map1, map2));
+        // list size equal and list values equal -> true
+        key = "list size equal and list values equal -> true";
+        map1.clear();
+        map1.put(key, new ArrayList<>(Arrays.asList(1, 2)));
+        map2.clear();
+        map2.put(key, new ArrayList<>(Arrays.asList(1, 2)));
+        assertTrue(MapUtils.multiValueMapEquals(map1, map2));
+        // list size equal and list values equal -> true
+        key = "list size equal and list values equal -> true 222";
+        map1.clear();
+        map1.put(key, new ArrayList<>(Arrays.asList(1, 2)));
+        map2.clear();
+        map2.put(key, new ArrayList<>(Arrays.asList("1", 2)));
+        assertTrue(MapUtils.multiValueMapEquals(map1, map2));
+        // list size equal and list values not equals -> false
+        key = "list size equal and list values not equals -> false";
+        map1.clear();
+        map1.put(key, new ArrayList<>(Arrays.asList(1, 2)));
+        map2.clear();
+        map2.put(key, new ArrayList<>(Arrays.asList(2, 3)));
+        assertFalse(MapUtils.multiValueMapEquals(map1, map2));
+    }
+
+    @Test
+    void multiStringValueMapEquals() {
+        // TODO
+    }
+}
