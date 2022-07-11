873c1b665b505d8e64a57285602b991ce751d3dc
taogen <taogenjia@gmail.com>
2022-07-11 16:43:10 +0800

    [feat] Add methods of ClassUtils

--- src/main/java/com/taogen/commons/reflection/ClassUtils.java
+++ src/main/java/com/taogen/commons/reflection/ClassUtils.java
@@ -2,12 +2,11 @@ package com.taogen.commons.reflection;
 
 import com.taogen.commons.datatypes.string.StringCaseUtils;
 
+import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Taogen
@@ -108,6 +107,29 @@ public class ClassUtils {
                 break;
             }
             classNode = classNode.getSuperclass();
+        }
+    }
+
+    public static Set<? extends Serializable> getFieldValuesFromList(List<?> list, String idFieldName) {
+        Set<Serializable> fieldValues = new HashSet<>();
+        for (Object obj : list) {
+            Object fieldValue = getFieldValueFromObject(obj, idFieldName);
+            if (fieldValue != null) {
+                fieldValues.add((Serializable) fieldValue);
+            }
+        }
+        return fieldValues != null ? fieldValues : Collections.emptySet();
+    }
+
+    public static void setFieldValuesForList(List<?> list, Map<String, String> idValueToFieldValue, String idField, String field) {
+        if (list == null) {
+            return;
+        }
+        for (Object obj : list) {
+            Object id = getFieldValueFromObject(obj, idField);
+            if (id != null) {
+                ClassUtils.setFieldValueToObject(obj, field, idValueToFieldValue.get(String.valueOf(id)));
+            }
         }
     }
 }
