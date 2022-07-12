9047cbb3f0a4bf017133c0fc75c9908546550b98
taogen <taogenjia@gmail.com>
2022-07-12 09:07:35 +0800

    [refactor] Optimize code. Add try catch

--- src/main/java/com/taogen/commons/crypto/HashUtils.java
+++ src/main/java/com/taogen/commons/crypto/HashUtils.java
@@ -8,32 +8,52 @@ import java.security.NoSuchAlgorithmException;
  * @author Taogen
  */
 public class HashUtils {
-    public static String md5(String source) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+    public static String md5(String source) {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
         messageDigest.reset();
         messageDigest.update(source.getBytes(StandardCharsets.UTF_8));
         byte[] digest = messageDigest.digest();
         return CryptoUtils.bytesToHexString(digest);
     }
 
-    public static String sha1(String source) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+    public static String sha1(String source) {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
         messageDigest.reset();
         messageDigest.update(source.getBytes(StandardCharsets.UTF_8));
         byte[] digest = messageDigest.digest();
         return CryptoUtils.bytesToHexString(digest);
     }
 
-    public static String sha256(String source) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+    public static String sha256(String source) {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
         messageDigest.reset();
         messageDigest.update(source.getBytes(StandardCharsets.UTF_8));
         byte[] digest = messageDigest.digest();
         return CryptoUtils.bytesToHexString(digest);
     }
 
-    public static String sha512(String source) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+    public static String sha512(String source) {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
         messageDigest.reset();
         messageDigest.update(source.getBytes(StandardCharsets.UTF_8));
         byte[] digest = messageDigest.digest();
