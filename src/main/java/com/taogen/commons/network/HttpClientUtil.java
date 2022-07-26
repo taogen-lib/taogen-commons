66c8f792dbf7b7ccdabb5650abbdbcb0e2176acd
taogen <taogenjia@gmail.com>
2022-07-26 15:09:37 +0800

    [feat] Add HttpClientUtil

---
+++ src/main/java/com/taogen/commons/network/HttpClientUtil.java
@@ -0,0 +1,140 @@
+package com.taogen.commons.network;
+
+import com.taogen.commons.collection.MultiValueMapUtils;
+import com.taogen.easyhttpclient.vo.*;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
+
+/**
+ * @author Taogen
+ */
+public class HttpClientUtil {
+    public static String MULTIPART_BODY_BOUNDARY = new BigInteger(256, new Random()).toString();
+
+    public static HttpResponse requestWithoutBody(HttpRequest request) throws IOException {
+        URL urlObj = getURL(request.getUrl(), request.getQueryParams());
+        HttpURLConnection urlConnection = (HttpURLConnection) urlObj.openConnection();
+        urlConnection.setRequestMethod(request.getMethod().name());
+        addHeaders(urlConnection, request.getHeaders());
+        try {
+            return getResponse(urlConnection);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    private static URL getURL(String url, Map<String, List<Object>> queryParams) throws MalformedURLException {
+        StringBuilder realUrl = new StringBuilder()
+                .append(url);
+        if (MultiValueMapUtils.isNotEmpty(queryParams)) {
+            realUrl.append("?")
+                    .append(HttpRequestUtil.multiValueMapToQueryString(queryParams))
+                    .toString();
+        }
+        return new URL(realUrl.toString());
+    }
+
+    private static void addHeaders(HttpURLConnection urlConnection, Map<String, List<Object>> headers) {
+        if (headers != null) {
+            for (Map.Entry<String, List<Object>> entry : headers.entrySet()) {
+                String key = entry.getKey();
+                List<Object> values = entry.getValue();
+                if (values != null) {
+                    for (Object value : values) {
+                        urlConnection.addRequestProperty(key, Objects.toString(value));
+                    }
+                }
+            }
+        }
+    }
+
+    private static HttpResponse getResponse(HttpURLConnection urlConnection) throws IOException {
+        HttpResponse httpResponse = new HttpResponse();
+        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        httpResponse.setBodyString(convertStreamToString(in));
+        httpResponse.setBody(httpResponse.getBodyString().getBytes(StandardCharsets.UTF_8));
+        httpResponse.setStatusCode(urlConnection.getResponseCode());
+        httpResponse.setHeaders(MultiValueMapUtils.multiStringValueMapToMultiObjectValueMap(urlConnection.getHeaderFields()));
+        return httpResponse;
+    }
+
+    private static String convertStreamToString(InputStream in) throws IOException {
+        byte[] buffer = new byte[1024];
+        int length;
+        try (
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ) {
+            while ((length = in.read(buffer)) != -1) {
+                out.write(buffer, 0, length);
+            }
+            return new String(out.toByteArray(), StandardCharsets.UTF_8);
+        }
+    }
+
+
+    public static HttpResponse requestWithJson(HttpRequestWithJson request) throws IOException {
+        URL urlObj = getURL(request.getUrl(), request.getQueryParams());
+        HttpURLConnection urlConnection = (HttpURLConnection) urlObj.openConnection();
+        urlConnection.setRequestMethod(request.getMethod().name());
+        addHeaders(urlConnection, request.getHeaders());
+        urlConnection.setRequestProperty("Content-Type", "application/json");
+        // Connection Will Be Used to Send Content
+        urlConnection.setDoOutput(true);
+        byte[] postData = request.getJson().getBytes(StandardCharsets.UTF_8);
+        urlConnection.setRequestProperty("Content-Length", String.valueOf(postData.length));
+        OutputStream out = urlConnection.getOutputStream();
+        out.write(postData);
+        try {
+            return getResponse(urlConnection);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public static HttpResponse requestWithUrlEncodedForm(HttpRequestWithForm request) throws IOException {
+        URL urlObj = getURL(request.getUrl(), request.getQueryParams());
+        HttpURLConnection urlConnection = (HttpURLConnection) urlObj.openConnection();
+        urlConnection.setRequestMethod(request.getMethod().name());
+        addHeaders(urlConnection, request.getHeaders());
+        urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        urlConnection.setDoOutput(true);
+        byte[] postData = HttpRequestUtil.multiValueMapToQueryString(request.getFormData())
+                .getBytes(StandardCharsets.UTF_8);
+        urlConnection.setRequestProperty("Content-Length", String.valueOf(postData.length));
+        OutputStream out = urlConnection.getOutputStream();
+        out.write(postData);
+        try {
+            return getResponse(urlConnection);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public static HttpResponse requestWithFormData(HttpRequestWithMultipart request) throws IOException {
+        URL urlObj = getURL(request.getUrl(), request.getQueryParams());
+        HttpURLConnection urlConnection = (HttpURLConnection) urlObj.openConnection();
+        urlConnection.setRequestMethod(request.getMethod().name());
+        addHeaders(urlConnection, request.getHeaders());
+        urlConnection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + MULTIPART_BODY_BOUNDARY);
+        urlConnection.setDoOutput(true);
+        byte[] postData = HttpRequestUtil.multiValueMapToMultipartData(request.getFormData(), MULTIPART_BODY_BOUNDARY);
+        urlConnection.setRequestProperty("Content-Length", String.valueOf(postData.length));
+        OutputStream out = urlConnection.getOutputStream();
+        out.write(postData);
+        try {
+            return getResponse(urlConnection);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}
---
+++ src/test/java/com/taogen/commons/network/HttpClientUtilTest.java
@@ -0,0 +1,153 @@
+package com.taogen.commons.network;
+
+import com.taogen.commons.io.FileUtils;
+import com.taogen.easyhttpclient.enums.HttpMethod;
+import com.taogen.easyhttpclient.mockwebserver.MockWebServerTest;
+import com.taogen.easyhttpclient.vo.*;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.function.Function;
+
+class HttpClientUtilTest {
+
+    protected static MockWebServer mockWebServer;
+
+    public static final String RANDOM_TOKEN = "123456";
+
+    public static final String CHINESE_TEST = "中文测试";
+    public static final String APP_HEADER_KEY = "my-app-id";
+    public static final String APP_HEADER_VALUE = "java-http-clients";
+
+    @BeforeEach
+    void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    void requestWithoutBody() throws InterruptedException {
+        Function<String, HttpRequest> getHttpRequest = url -> {
+            Map<String, List<Object>> queryParams = new HashMap<>();
+            queryParams.put("id", Arrays.asList(1));
+            queryParams.put("name", Arrays.asList("test"));
+            queryParams.put("testChinese", Arrays.asList(CHINESE_TEST));
+            HttpRequest httpRequest = HttpRequest.builder()
+                    .url(url)
+                    .method(HttpMethod.GET)
+                    .headers(getBasicHeaders())
+                    .queryParams(queryParams)
+                    .build();
+            return httpRequest;
+        };
+        Function<HttpRequest, HttpResponse> getResponse = httpRequest -> {
+            try {
+                HttpResponse httpResponse = HttpClientUtil.requestWithoutBody(httpRequest);
+                return httpResponse;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        MockWebServerTest.testRequestWithoutBody(mockWebServer, getHttpRequest, getResponse);
+    }
+
+    @Test
+    void requestWithJson() throws InterruptedException {
+        Function<String, HttpRequestWithJson> getHttpRequest = url -> {
+            String requestBody = "{\"id\": 1}";
+            return HttpRequestWithJson.builder()
+                    .url(url)
+                    .method(HttpMethod.POST)
+                    .headers(getBasicHeaders())
+                    .json(requestBody)
+                    .build();
+        };
+
+        Function<HttpRequestWithJson, HttpResponse> getResponse = httpRequest -> {
+            try {
+                return HttpClientUtil.requestWithJson(httpRequest);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        MockWebServerTest.testPostWithJson(mockWebServer, getHttpRequest, getResponse);
+    }
+
+    @Test
+    void requestWithUrlEncodedForm() throws InterruptedException {
+        Function<String, HttpRequestWithForm> getHttpRequest = url -> {
+            Map<String, List<Object>> formData = new HashMap<>();
+            formData.put("id", Arrays.asList(1));
+            formData.put("name", Arrays.asList("test", "test2"));
+            return HttpRequestWithForm.builder()
+                    .url(url)
+                    .method(HttpMethod.POST)
+                    .headers(getBasicHeaders())
+                    .formData(formData)
+                    .build();
+        };
+
+        Function<HttpRequestWithForm, HttpResponse> getResponse = httpRequest -> {
+            try {
+                return HttpClientUtil.requestWithUrlEncodedForm(httpRequest);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        MockWebServerTest.testPostWithUrlEncodedForm(mockWebServer, getHttpRequest, getResponse);
+
+    }
+
+    @Test
+    void requestWithFormData() throws IOException, InterruptedException {
+        Function<String, HttpRequestWithMultipart> getHttpRequest = url -> {
+            Map<String, List<Object>> formData = new HashMap<>();
+            formData.put("id", Arrays.asList(1));
+            formData.put("name", Arrays.asList("test", "test2"));
+            try {
+                formData.put("file", Arrays.asList(
+                        new File(FileUtils.getFilePathByFileClassPath("static/test.jpg")),
+                        new File(FileUtils.getFilePathByFileClassPath("static/test.txt"))));
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            return HttpRequestWithMultipart.builder()
+                    .url(url)
+                    .method(HttpMethod.POST)
+                    .headers(getBasicHeaders())
+                    .formData(formData)
+                    .build();
+        };
+
+        Function<HttpRequestWithMultipart, HttpResponse> getResponse = httpRequest -> {
+            try {
+                return HttpClientUtil.requestWithFormData(httpRequest);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        MockWebServerTest.testPostWithMultipartForm(mockWebServer, getHttpRequest, getResponse);
+
+    }
+
+    protected Map<String, List<Object>> getBasicHeaders() {
+        Map<String, List<Object>> headers = new LinkedHashMap<>();
+        headers.put(APP_HEADER_KEY, new ArrayList<>(Arrays.asList(APP_HEADER_VALUE)));
+        return headers;
+    }
+
+}
