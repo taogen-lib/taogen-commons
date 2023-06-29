e36dccc05f97e5ba00bdee1da774e0fe4088c794
taogen <taogenjia@gmail.com>
2023-06-29 15:21:11 +0800

    [feat] Add method getSubListByMaxCharNum

--- commons-lang/src/main/java/com/taogen/commons/collection/CollectionUtils.java
+++ commons-lang/src/main/java/com/taogen/commons/collection/CollectionUtils.java
@@ -1,6 +1,7 @@
 package com.taogen.commons.collection;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Taogen
@@ -12,5 +13,18 @@ public class CollectionUtils {
 
     public static boolean isNotEmpty(Collection collection) {
         return collection != null && !collection.isEmpty();
+    }
+
+    private static List<String> getSubListByMaxCharNum(List<String> list, Integer maxCharNum) {
+        int charNumSum = 0;
+        int index = list.size();
+        for (int i = 0; i < list.size(); i++) {
+            charNumSum += list.get(i).length();
+            if (charNumSum > maxCharNum) {
+                index = i;
+                break;
+            }
+        }
+        return list.subList(0, index);
     }
 }
