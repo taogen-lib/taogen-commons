bdb5190b6946261850328141564a3d16739c2355
taogen <taogenjia@gmail.com>
2023-09-18 15:27:13 +0800

    [refactor] Add method getBasicHeaders

--- httpclient-common/src/main/java/com/taogen/commons/httpclient/vo/HttpRequest.java
+++ httpclient-common/src/main/java/com/taogen/commons/httpclient/vo/HttpRequest.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,4 +26,13 @@ public class HttpRequest {
      * URL query string
      */
     private Map<String, List<Object>> queryParams;
+
+    public static Map<String, List<Object>> getBasicHeaders() {
+        Map<String, List<Object>> headers = new HashMap<>();
+        headers.put("Accept", Arrays.asList("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"));
+        headers.put("Accept-Encoding", Arrays.asList("gzip, deflate, br"));
+        headers.put("Accept-Language", Arrays.asList("en-US,en,zh;q=0.5"));
+        headers.put("User-Agent", Arrays.asList("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"));
+        return headers;
+    }
 }
