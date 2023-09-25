30fec35dd6a9fd85ecd34124b18379c71777b20f
taogen <taogenjia@gmail.com>
2023-09-25 18:48:45 +0800

    [feat] Add methods to StringUtils

--- commons-lang/src/main/java/com/taogen/commons/datatypes/string/StringUtils.java
+++ commons-lang/src/main/java/com/taogen/commons/datatypes/string/StringUtils.java
@@ -85,6 +85,15 @@ public class StringUtils {
     }
 
 
+    public static boolean isBlank(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
+    public static boolean isNotBlank(String str) {
+        return str != null && str.trim().length() > 0;
+    }
+
+
     // >> string verification
 
 
